3435b651d6d152f39d812bfacf3cb56844fe416e
jh92.mun <jh92.mun@samsung.com>
2025-11-16 23:53:13 +0900

    initial commit

---
+++ src/emm/_core.py
@@ -0,0 +1,59 @@
+"""
+
+
+THIS FILE IS PART OF EMM LIBRARY
+"""
+
+import os
+import sys
+import numpy as np
+
+
+
+
+
+def load(
+    name:str,
+    w:np.ndarray
+  ):
+  pass
+
+
+# interp
+
+# warning for out-of-bounds
+
+# export raw data (with unit/param conversion)
+
+
+
+
+
+
+
+def _read():
+  pass
+
+
+
+
+
+def _unitconv():
+  pass
+"""
+Hz, GHz, THz, 
+eV
+nm, um, m
+
+"""
+
+def _paramconv():
+  pass
+"""
+e
+n
+"""
+
+
+
+
