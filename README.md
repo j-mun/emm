b0b0aacdd0829e81a0f5f17c3573816da53a558b
jh92.mun <jh92.mun@samsung.com>
2025-11-27 21:27:58 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# emm
+# emm | A Python electromagnetic material manager
+
+This package helps managing tabulated electromagnetic material properties (refractive index or permittivity). 
+
+# Features
+
+- 
+
+
+# Installation
+
+```bash
+python -m pip install .
+```
+
+# Usages
+
+## 
+
+```python
+import emm
 
 
 
-
+```
--- tests/test.py
+++ tests/test.py
@@ -10,7 +10,7 @@ emm.avail('Ag')
 
 
 
-f,e = emm.read('Ag\\Johnson.yaml', 'm','e')
+f,e = emm.read('Ge/Aspnes', 'm','e')
 
 plt.figure()
 plt.plot(f, np.real(e))
@@ -18,5 +18,21 @@ plt.plot(f, np.imag(e))
 plt.show()
 
 
+
 f = np.linspace(400e-9, 900e-9, 101)
-e = emm.load('Ag/Johnson', )
+n = emm.load('Ag/Johnson', f)
+
+plt.figure()
+plt.plot(f, np.real(n))
+plt.plot(f, np.imag(n))
+plt.show()
+
+
+
+f = np.linspace(400e-9, 900e-9, 101)
+n = emm.load('Ag/Johnson', f, interp='cubic')
+
+plt.figure()
+plt.plot(f, np.real(n))
+plt.plot(f, np.imag(n))
+plt.show()
